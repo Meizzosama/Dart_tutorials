4231543f1981feccc88af3d1c5d938821e547332
Muhammad Osama Noor <mosamanoor17@gmail.com>
2022-10-25 00:56:17 +0500

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main() {
+  //About Boolean Data Type
+  int num1 = 10;
+  int num2 = 10;
+  bool isEven;
+  bool result = true;
+  bool result1 = true;
+  isEven = num2 / num1 == 1;
+  result = num2 / 2 == 5;
+  result1 = num1 / 2 == 5;
+  print(result);
+  print(result1);
+  print(isEven);
+}
